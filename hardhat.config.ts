073996667b3196dfa5fe5c4de35d2918ed882cbb
“senn” <sennhanami@163.com>
2023-02-28 18:03:23 +0800

    none

---
+++ hardhat.config.ts
@@ -0,0 +1,10 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-ethers";
+import "@openzeppelin/hardhat-upgrades";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.17",
+};
+
+export default config;
